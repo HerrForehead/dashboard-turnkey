658a853eadc85b3eac64b4d8c34a6d5ff81e4a71
Sven Verstegen <489755@student.fontys.nl>
2023-06-09 14:05:47 +0200

    update login

--- app.py
+++ app.py
@@ -4,48 +4,6 @@ import subprocess
 from pyVim import connect
 from flask import Flask, render_template, request, redirect
 
-# Create the app
-app = Flask(__name__)
-
-# Force redirect to HTTPS
-@app.before_request
-def force_https():
-    if request.endpoint in app.view_functions and not request.is_secure:
-        return redirect(request.url.replace('http://', 'https://'))
-
-
-# Renders the home page
-@app.route('/')
-def start():
-    return render_template("login.html")
-
-# Renders the home page
-@app.route('/home')
-def home():
-    return render_template("login.html")
-
-# Grabs the data upon clicking the submit button and puts it in a dictionary
-@app.route("/result", methods=['POST', "GET"])
-def result():
-    output = request.form.to_dict()
-    print(output)
-    return render_template("result.html")
-
-
-@app.route("/login", methods=['POST', "GET"])
-def login():
-    credentials = request.form.to_dict()
-    print(credentials)
-
-    retrieve_network_devices(credentials['email'], credentials['password'])
-
-
-    return render_template("login.html")
-
-# Run the app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True, ssl_context=('/home/student/dashboard-turn-key/cert.pem', '/home/student/dashboard-turn-key/key.pem'))
-
 def print_network(network, level):
     indent = '  ' * level
     print(f"{indent}Network: {network.name}")
@@ -97,3 +55,46 @@ def retrieve_network_devices(username, password):
     finally:
         # Disconnect from vSphere
         connect.Disconnect(service_instance)
+
+# Create the app
+app = Flask(__name__)
+
+# Force redirect to HTTPS
+@app.before_request
+def force_https():
+    if request.endpoint in app.view_functions and not request.is_secure:
+        return redirect(request.url.replace('http://', 'https://'))
+
+
+# Renders the home page
+@app.route('/')
+def start():
+    return render_template("login.html")
+
+# Renders the home page
+@app.route('/home')
+def home():
+    return render_template("login.html")
+
+# Grabs the data upon clicking the submit button and puts it in a dictionary
+@app.route("/result", methods=['POST', "GET"])
+def result():
+    output = request.form.to_dict()
+    print(output)
+    return render_template("result.html")
+
+
+@app.route("/login", methods=['POST', "GET"])
+def login():
+    credentials = request.form.to_dict()
+    print(credentials)
+
+    retrieve_network_devices(credentials['email'], credentials['password'])
+
+
+    return render_template("login.html")
+
+# Run the app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True, ssl_context=('/home/student/dashboard-turn-key/cert.pem', '/home/student/dashboard-turn-key/key.pem'))
+
