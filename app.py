1740470912393d12d8fd3c718b22cb7d7552b579
Sven Verstegen <489755@student.fontys.nl>
2023-06-09 13:57:52 +0200

    Add network check

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import threading
 import subprocess
+from pyVim import connect
 from flask import Flask, render_template, request, redirect
 
 # Create the app
@@ -16,12 +17,12 @@ def force_https():
 # Renders the home page
 @app.route('/')
 def start():
-    return render_template("index.html")
+    return render_template("login.html")
 
 # Renders the home page
 @app.route('/home')
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
 # Grabs the data upon clicking the submit button and puts it in a dictionary
 @app.route("/result", methods=['POST', "GET"])
@@ -33,15 +34,66 @@ def result():
 
 @app.route("/login", methods=['POST', "GET"])
 def login():
-    output = request.form.to_dict()
-    print(output)
+    credentials = request.form.to_dict()
+    print(credentials)
+
+    retrieve_network_devices(credentials['email'], credentials['password'])
+
+
     return render_template("login.html")
-
-
-
-
-    return render_template("index.html")
 
 # Run the app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, ssl_context=('/home/student/dashboard-turn-key/cert.pem', '/home/student/dashboard-turn-key/key.pem'))
+
+def print_network(network, level):
+    indent = '  ' * level
+    print(f"{indent}Network: {network.name}")
+
+    # Check if the network has child folders
+    if hasattr(network, 'childEntity'):
+        # Retrieve and print the child folders
+        child_folders = network.childEntity
+        for child_folder in child_folders:
+            print_network(child_folder, level + 1)
+
+def retrieve_network_devices(username, password):
+    try:
+        # Connect to vSphere
+        service_instance = connect.SmartConnect(
+            host="vcenter.netlab.fhict.nl",
+            user=username,
+            pwd=password
+        )
+
+        # Retrieve the content
+        content = service_instance.RetrieveContent()
+
+        # Get the root folder
+        root_folder = content.rootFolder
+
+        # Retrieve all datacenters in the vSphere inventory
+        datacenters = root_folder.childEntity
+        for datacenter in datacenters:
+            # Retrieve the network folder of the datacenter
+            network_folder = datacenter.networkFolder
+
+            # Retrieve the vDS-VLAN folder
+            vds_vlan_folder = None
+            for child_folder in network_folder.childEntity:
+                if child_folder.name == 'vDS-VLAN':
+                    vds_vlan_folder = child_folder
+                    break
+
+            # If vDS-VLAN folder is found, retrieve networks inside it
+            if vds_vlan_folder:
+                networks = vds_vlan_folder.childEntity
+                for network in networks:
+                    print_network(network, 0)
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+    finally:
+        # Disconnect from vSphere
+        connect.Disconnect(service_instance)
