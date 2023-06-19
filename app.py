85f8b5eb1a783692b5899109d54201265eada37e
Sven Verstegen <489755@student.fontys.nl>
2023-06-19 12:39:12 +0200

    ddd

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def configure():
         ansiblecommand = "ansible-playbook -i inventory.ini /home/student/turn-key-roll-out-playbooks/setupK8SVM.yml --extra-vars 'vm_net_name={network} vm_disk_gb={disk_space} vm_hw_cpu_n={num_cores} vm_hw_ram_mb={ram} vcenter_username={username} vcenter_password={password}'"
 
         # Run the ansible playbook using os.system and pass the form data as extra variables in a thread
-        threading.Thread(target=os.system, args=(f'ansiblecommand={ansiblecommand}; /bin/bash -c "$ansiblecommand"',)).start()
+        threading.Thread(target=os.system, args=(f'ansiblecommand="{ansiblecommand}"; /bin/bash -c "$ansiblecommand"',)).start()
 
         # Render the result page with the received data
         return render_template('result.html', disk_space=disk_space, num_cores=num_cores, ram=ram, network=network, username=username)
